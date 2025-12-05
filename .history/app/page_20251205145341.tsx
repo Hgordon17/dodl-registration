bb82f3c52e441d7251fabf3c618cceaf606229a5
henrygordon <henrygordon.jr@gmail.com>
2025-12-05 15:35:00 +1000

    navmanu

--- .history/app/page_20251205145341.tsx
+++ .history/app/page_20251205145341.tsx
@@ -14,20 +14,19 @@ import {
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
       <main className="flex min-h-screen w-full max-w-full max flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Button>Register</Button>
 
